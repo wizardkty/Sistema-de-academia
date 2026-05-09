b33f6fb8b15646aea407bc76b065b7de672356d1
Juan <juanruan659@gmail.com>
2026-05-09 01:20:17 -0300

    adicionando o src correto no projeto

---
+++ Av2_Projeto/src/models/CartaoCredito.java
@@ -0,0 +1,15 @@
+package models;
+
+public class CartaoCredito extends Cartao {
+	private byte quantParcela;
+
+	public byte getQuantParcela() {
+		return quantParcela;
+	}
+
+	public void setQuantParcela(byte quantParcela) {
+		this.quantParcela = quantParcela;
+	}
+	
+	
+}
---
+++ Av2_Projeto/src/models/Plano.java
@@ -0,0 +1,41 @@
+package models;
+
+public class Plano {
+	private int nomePlano;
+	private float valor;
+	private int duracao;
+	private String descricao;
+	
+	public void CalculaVencimento() {
+
+	}
+	
+	public int getNomePlano() {
+		return nomePlano;
+	}
+	public void setNomePlano(int nomePlano) {
+		this.nomePlano = nomePlano;
+	}
+	public float getValor() {
+		return valor;
+	}
+	public void setValor(float valor) {
+		this.valor = valor;
+	}
+	public int getDuracao() {
+		return duracao;
+	}
+	public void setDuracao(int duracao) {
+		this.duracao = duracao;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+	
+	
+}
