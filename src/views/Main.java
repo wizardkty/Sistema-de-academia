df0a3df588260ca51fa8b1a359c3b1804b13e30a
Juan <juanruan659@gmail.com>
2026-05-11 23:00:18 -0300

    Foi alterado a main no views, agr está como null e pode posicionar botões e qualquer coisa em qualquer parte do layout, também adicionei um botão mais como forma de teste

--- src/views/Main.java
+++ src/views/Main.java
@@ -1,14 +1,16 @@
 package views;
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.FlowLayout;
 import javax.swing.JLabel;
 
+import javax.swing.JFrame;
+import java.awt.Panel;
+import java.awt.Button;
 
-public class Main extends javax.swing.JFrame {
+public class Main extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
@@ -38,10 +40,15 @@ public class Main extends javax.swing.JFrame {
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		contentPane.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Academia");
+		lblNewLabel.setBounds(424, 11, 54, 30); // x, y, largura, altura
 		contentPane.add(lblNewLabel);
+		
+		Button button = new Button("New button");
+		button.setBounds(390, 120, 120, 30); // x, y, largura, altura
+		contentPane.add(button);
 
 	}
 
